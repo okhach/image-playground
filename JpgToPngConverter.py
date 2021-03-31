f6ba0ccef5ba08eb8ef1aabd4078cb04a9382368
Doitbig <1OAs2123n@g.com>
2021-03-31 09:41:58 +0800

    2

--- JpgToPngConverter.py
+++ JpgToPngConverter.py
@@ -3,9 +3,11 @@ import os
 from PIL import Image
 
 # grab the first and second argument
+first = sys.argv[1]
+second = sys.argv[2]
 
 #check if new\ exists, if not creat it
-
+if os.path.isdir(second) 
 #loop through POkedex
 
 #then convert JPG to PNG
